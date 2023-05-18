7e192789a6331a66850ec71569c68074fe04c4f0
Troy Chaplin <troy.chaplin@gmail.com>
2023-05-18 08:13:19 -0400

    Cleaned up files

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'ce273c6ee9463f3d6b4b');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '6a97846eff2f221892dd');
